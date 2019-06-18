2635e04bf0b3e4be3df539277e94c7b29a3afd89
Bugrahan Akbulut <b21426529@cs.hacettepe.edu.tr>
2019-06-18 18:36:06 +0300

    project initialization

---
+++ Assets/Scripts/RacketController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketController : MonoBehaviour {
+
+    private float _racketSpeed; 
+
+	// Use this for initialization
+	void Start () {
+        InitRacket();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        InputController();
+	}
+
+    private void InitRacket()
+    {
+        _racketSpeed = 7.5f;
+    }
+
+    void InputController()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        // Vector3 movementVector = Vector3.right * horizontalInput * _racketSpeed * Time.deltaTime;
+        // transform.Translate(movementVector);
+        GetComponent<Rigidbody2D>().velocity = Vector3.right * horizontalInput * _racketSpeed;
+    }
+}

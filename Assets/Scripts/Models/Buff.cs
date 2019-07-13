d3a3e5e442fa51c208cb7eb29dca9747da2e7fcb
Bugrahan Akbulut <akbulutbugrahan@gmail.com>
2019-07-13 21:54:01 +0300

    Added  Generic Buff Implementation and ExtendBuff Implementation

---
+++ Assets/Scripts/Models/Buff.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Buff : MonoBehaviour, Movable
+{
+
+    private float _speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitBuff();
+    }
+
+    // Update is called once per frame
+    
+    void InitBuff()
+    {
+        _speed = 2.0f;
+    }
+
+    public void MovementController()
+    {
+        this.GetComponent<Rigidbody2D>().velocity = Vector3.down * _speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Racket")
+            this.ExecuteBuff(other.gameObject);
+    }
+
+    public abstract void ExecuteBuff(GameObject racket);
+}
---
+++ Assets/Scripts/Models/ExtendBuff.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtendBuff : Buff
+{
+    public override void ExecuteBuff(GameObject racket)
+    {
+        racket.transform.localScale += Vector3.right;
+    }
+}
